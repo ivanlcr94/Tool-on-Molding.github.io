911949d995c67ad8bc6412c9e31b876069451550
Ivan Romero <ivanlcr94@gmail.com>
2024-03-03 20:57:45 -0300

    Mejorando calculadora
    Agregando modal al resultado

--- js/calculadoraCarga.js
+++ js/calculadoraCarga.js
@@ -1,40 +1,4 @@
-//array de maquinas
 
-const maquinas = [{
-        "id": 1,
-        "nombre": "Dakumar 600-HH",
-        "diametroHusillo": 72,
-        "maxCargacm3": 1282
-    },
-
-    {
-        "id": 2,
-        "nombre": "Haitian HTF 530X",
-        "diametroHusillo": 90,
-        "maxCargacm3": 2799
-    },
-
-    {
-        "id": 3,
-        "nombre": "Dakumar 650-SV",
-        "diametroHusillo": 95,
-        "maxCargacm3": 3046
-    },
-
-    {
-        "id": 4,
-        "nombre": "Haitian HTF 530X",
-        "diametroHusillo": 90,
-        "maxCargacm3": 2799
-    },
-
-    {
-        "id": 5,
-        "nombre": "Haitian 470W",
-        "diametroHusillo": 84,
-        "maxCargacm3": 2050
-    }
-]
 
 
 document.querySelector("#formulario").addEventListener("submit", calcular)
@@ -48,14 +12,14 @@ function calcular(e) {
     const pesoCavidad = document.querySelector(".pesoCavidad").value;
     const cantidadCavidad = document.querySelector(".cavidades").value;
     const pesoColada = document.querySelector(".pesoColada").value;
-    const seleccionarMaquina = document.querySelector(".seleccionarMaquina").value;
-    const diametroHusillo = 
+    const diametroHusillo = document.querySelector(".diametroHusillo").value;
+    const cargaMaxCm3 = document.querySelector(".cargaMaxCm3").value;
     const seleccionarMp = document.querySelector(".seleccionarMp").value;
     const ciclo = document.querySelector(".ciclo").value;
 
     //Filtro array para seleccionar maquina
 
-    let selectorDeMaquina = maquinas.filter(elemento => elemento.id == seleccionarMaquina).find(object => object);
+    //let selectorDeMaquina = maquinas.filter(elemento => elemento.id == seleccionarMaquina).find(object => object);
 
     //calcular
 
@@ -65,13 +29,13 @@ function calcular(e) {
     let volumenDosificacion = pesoDePrensada / seleccionarMp;
 
     //! ÁREA DE HUSILLO
-    let areaDeHusillo = Math.PI * (selectorDeMaquina.diametroHusillo / 10 * selectorDeMaquina.diametroHusillo / 10) / 4;
+    let areaDeHusillo = Math.PI * (diametroHusillo / 10 * diametroHusillo / 10) / 4;
 
     //! RECORRIDO DE DOSIFICACIÓN
     let recorridoDosificacion = (volumenDosificacion / areaDeHusillo) * 10;
     
     //! % UTILIZACION UNIDAD INYECCIÓN
-    let utilizacionUnidadInyección = (volumenDosificacion / selectorDeMaquina.maxCargacm3) * 100;
+    let utilizacionUnidadInyección = (volumenDosificacion / cargaMaxCm3) * 100;
 
     let cojin;
 
@@ -81,7 +45,7 @@ function calcular(e) {
         cojin = 0.10;
     }
 
-    let recorridoCojin = selectorDeMaquina.diametroHusillo * cojin;
+    let recorridoCojin = diametroHusillo * cojin;
 
     let volumenCojin = recorridoCojin / 10 * areaDeHusillo;
     
@@ -90,16 +54,20 @@ function calcular(e) {
 
     let recorridoTotalInyeccion = recorridoDosificacion + recorridoCojin;
     let volumenTotalDosificacion = volumenDosificacion+volumenCojin;
-    let utilizacionTotalUnidadInyección = (volumenTotalDosificacion / selectorDeMaquina.maxCargacm3) * 100;
-    let tiempoResidencia = 2* (selectorDeMaquina.maxCargacm3/volumenDosificacion) * (parseFloat(ciclo)/60);
+    let utilizacionTotalUnidadInyección = (volumenTotalDosificacion / cargaMaxCm3) * 100;
+    let tiempoResidencia = 2* (cargaMaxCm3/volumenDosificacion) * (parseFloat(ciclo)/60);
     let volumenConmutacion = puntoConmutacion / 10 * areaDeHusillo;
 
     // Resultado al html
+
+    
+
+
     contenedorResultado.innerHTML = "";
     let div = document.createElement("div");
-    div.classList.add('cuadroResultado', 'shadow');
+    div.classList.add('cuadroResultado');
 
-    div.innerHTML = `<h2>Resultado</h2>
+    div.innerHTML = `
         <p>Dosificación: ${recorridoTotalInyeccion.toFixed(1)}  mm (${volumenTotalDosificacion.toFixed(1)} cm3)</p>
         <p>Conmutacción: ${puntoConmutacion.toFixed(1)} mm (${volumenConmutacion.toFixed(1)}cm3)</p>
         <p>Cojín: ${recorridoCojin.toFixed(1)} mm (${volumenCojin.toFixed(1)} cm3)</p>
